cfc00fe309aa1b68169845f631427d6f59ae89d3
Michael Adan <electric_boarder@hotmail.com>
2022-10-02 18:53:40 -0600

    Added feature to map over array and add numbers to dice elements.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Die from "./Die"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [die, setDie] = useState(allNewDice())
 
 function allNewDice() {
  const randomNumbers = [];
@@ -13,20 +13,13 @@ function allNewDice() {
  return randomNumbers
 }
 
+const dice = die.map(dice => <Die value={dice} /> )
+
   return (
     <div className='screen'>
     <div className="App">
       <main>
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
+        {dice}
       </main>
     </div>
     </div>
